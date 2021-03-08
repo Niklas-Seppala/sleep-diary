77ad505d12bf434619b1f8941795e537dd5dbb74
anne-chin <an-an.chin@metropolia.fi>
2021-03-08 09:44:19 +0200

    Questionnaire activity edited.

--- app/src/main/java/com/example/sleepdiary/QuetionnaireActivity.java
+++ app/src/main/java/com/example/sleepdiary/QuetionnaireActivity.java
@@ -5,82 +5,121 @@ import android.os.Bundle;
 import com.example.sleepdiary.data.db.DbConnection;
 import com.example.sleepdiary.data.models.Rating;
 import com.example.sleepdiary.data.models.SleepEntry;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.sleepdiary.data.GlobalData;
+
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import com.example.sleepdiary.data.models.User;
+import com.example.sleepdiary.time.DateTime;
+
 
 public class QuetionnaireActivity extends AppCompatActivity {
 
-    Date startTime = new Date();
-    Date endTime = new Date();
+    TextView heading;
+    TextView sleepDuration_tv;
+    TextView duration_tv;
+    TextView q1;
+    RadioGroup radioGroupQ1;
+    TextView q3;
+    EditText editNumber = (EditText) findViewById(R.id.editNumber);
+    TextView q3_2;
+    Button submitBtn;
+    Rating rating;
+    int startTimeStamp;
+    int endTimeStamp;
+    int caffeinIntake;
 
+
+    SleepEntry newSleepEntry = new SleepEntry();
+
+/*Initial activity view*/
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quetionnaire);
 
-        getDifference();
+        heading = (TextView) findViewById(R.id.heading02_tv);
+        sleepDuration_tv = (TextView) findViewById(R.id.sleepDuration_tv);
+        duration_tv = (TextView) findViewById(R.id.duration_tv);
+        q1 = (TextView) findViewById(R.id.q1);
+        radioGroupQ1 = (RadioGroup) findViewById(R.id.radioGroupQ1);
+        q3 = (TextView) findViewById(R.id.q3);
+        q3_2 = (TextView) findViewById(R.id.q3_2);
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(submit);
+        radioGroupQ1 = findViewById(R.id.radioGroupQ1);
+
+        getDuration();
+
     }
 
-    public void getDifference(){
-        SimpleDateFormat format = new SimpleDateFormat("dd hh:mm a");
-        Calendar calendar = Calendar.getInstance();
+    /*Get the duration of sleep by timestamps*/
+    public void getDuration() {
+        startTimeStamp = newSleepEntry.getStartTimestamp();
+        endTimeStamp = newSleepEntry.getEndTimestamp();
+        int duration = endTimeStamp - startTimeStamp;
+        int hours = DateTime.getHoursFromSeconds(duration);
+        int minutes = DateTime.getMinutesFromSeconds(duration);
+        duration_tv.setText(getString(R.string.time_h_min_dur_long, hours, minutes));
 
-        try {
-            startTime = format.parse("08:00 PM");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        try {
-            endTime = format.parse("04:00 AM");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-
-        long difference = startTime.getTime() - endTime.getTime();
-        int days = (int) (difference / (1000*60*60*24));
-        int hours = (int) ((difference - (1000*60*60*24*days)) / (1000*60*60));
-        int min = (int) (difference - (1000*60*60*24*days) - (1000*60*60*hours)) / (1000*60);
-
-        Log.i("log_tag","Hours: "+hours+", Mins: "+min);
-
-        String setDurationTv = "Hours: "+hours+", Mins: "+min;
-        TextView duration_tv = (TextView)findViewById(R.id.duration_tv);
-        duration_tv.setText(setDurationTv);
     }
 
+    /* submitting the questionnaire*/
+    private View.OnClickListener submit = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            q3();
+            onEmoSelected(radioGroupQ1,0);
+            saveEntry();
 
-    public void onEmoSelected(View view){
+        }
+    };
+
+/* Radiogroup, return sleep quality to int 0-5*/
+        public void onEmoSelected (RadioGroup group,int checkedId){
         RadioGroup radioGroup = findViewById(R.id.radioGroupQ1);
-        switch (radioGroup.getCheckedRadioButtonId()){
+
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.emoVerySatisfied_q1:
+                rating = Rating.fromInt(5);
+                break;
             case R.id.emoSatisfied_q1:
+                rating = Rating.fromInt(4);
                 break;
-            case R.id.emoSmile_q1:
+            case R.id.emoNeutral_q1:
+                rating = Rating.fromInt(3);
                 break;
-            case R.id.emoNull_q1:
+            case R.id.emoDissatisfied_q1:
+                rating = Rating.fromInt(2);
                 break;
-            case R.id.emoGrimacing_q1:
+            case R.id.emoVeryDissatisfied_q1:
+                rating = Rating.fromInt(1);
                 break;
-            case R.id.emoDead_q1:
+            default:
+                rating = Rating.fromInt(0);
                 break;
+
         }
+
     }
 
+/* Read user input*/
+    public void q3() {
+
+        editNumber = (EditText) findViewById(R.id.editNumber);
+        caffeinIntake = Integer.parseInt(editNumber.getText().toString());
+    }
+
+    /*Save entry to app's DB*/
     public void saveEntry() {
+
+        User user = GlobalData.getInstance().getCurrentUser();
         DbConnection db = new DbConnection(this);
 
         SleepEntry entry = new SleepEntry(1, Rating.UNDEFINED, 123135, -1);
@@ -89,25 +128,4 @@ public class QuetionnaireActivity extends AppCompatActivity {
         db.close();
     }
 
-    public void onEmoSelected2(View view){
-        RadioGroup radioGroup = findViewById(R.id.radioGroupQ2);
-        switch (radioGroup.getCheckedRadioButtonId()){
-            case R.id.emoSatisfied_q2:
-                break;
-            case R.id.emoSmile_q2:
-                break;
-            case R.id.emoNull_q2:
-                break;
-            case R.id.emoGrimacing_q2:
-                break;
-            case R.id.emoDead_q2:
-                break;
-        }
-    }
-
-    public void q3(View view) {
-
-        EditText editText1 = (EditText) findViewById(R.id.editNumber);
-        int awakenings = Integer.parseInt(editText1.getText().toString());
-    }
-}
+ }
