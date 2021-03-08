d1e2877198bb2cd0a1863a87ed078c9b6e40249b
HennaPe <Moi>
2021-03-08 11:59:32 +0200

    add random tips view from item list

--- app/src/main/java/com/example/sleepdiary/InfoFragment.java
+++ app/src/main/java/com/example/sleepdiary/InfoFragment.java
@@ -1,19 +1,33 @@
 package com.example.sleepdiary;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Random;
+
+
 public class InfoFragment extends Fragment {
     // Use this :)
+    private TextView text;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        text = (TextView) view.findViewById(R.id.textView3);
+        String[] tips = getResources().getStringArray(R.array.tips_array);
+        // Get a random item from the list
+        Random random = new Random();
+        text.setText(tips[random.nextInt(tips.length)]);
     }
 
     // Dont touch!
