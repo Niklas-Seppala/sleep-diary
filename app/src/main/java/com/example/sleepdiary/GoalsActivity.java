a5db9b7aa73ad1aa2f246fc08125303ff315f892
Niklas Seppälä <niklas.t.seppala@gmail.com>
2021-03-08 10:09:05 +0200

    refactor, extract methods

--- app/src/main/java/com/example/sleepdiary/GoalsActivity.java
+++ app/src/main/java/com/example/sleepdiary/GoalsActivity.java
@@ -16,6 +16,9 @@ import com.example.sleepdiary.data.models.User;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * In this view, users can modify their private goals.
+ */
 public class GoalsActivity extends AppCompatActivity {
     private Button submitBtn;
     private TextView hoursInput;
@@ -28,9 +31,12 @@ public class GoalsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_goals);
 
         findViews();
-        setSubmitClickHander();
+        submitBtn.setOnClickListener(v -> trySubmitChanges());
     }
 
+    /**
+     * Find user inputs in the goals view.
+     */
     private void findViews() {
         submitBtn = findViewById(R.id.goals_submit_btn);
         hoursInput = findViewById(R.id.goals_hours_input);
@@ -38,6 +44,43 @@ public class GoalsActivity extends AppCompatActivity {
         caffeineInput = findViewById(R.id.goals_caffeine_input);
     }
 
+    private void trySubmitChanges() {
+        if (!validate()) return;
+
+        // Parse values and change them to correct format (time is in seconds)
+        int hoursInSeconds = Integer.parseInt(hoursInput.getText().toString()) * 60 * 60;
+        int minsInSeconds = Integer.parseInt(minsInput.getText().toString()) * 60;
+        int totalSeconds = hoursInSeconds + minsInSeconds;
+        int cup = Integer.parseInt(caffeineInput.getText().toString());
+
+        // Create new user data model and update it based on old and new values
+        User currentUser = GlobalData.getInstance().getCurrentUser();
+        User newUser = new User(currentUser.getId(), currentUser.getName(), totalSeconds, cup);
+
+        // Update database
+        DbConnection db = new DbConnection(this);
+        String[] params = { Integer.toString(currentUser.getId()) };
+        db.update(DbTables.user.TABLE_NAME, newUser, "id=?", params);
+        db.close();
+
+        // Display success message
+        Toast toast = Toast.makeText(this, "Goals updated", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL,
+                0, 190);
+        toast.show();
+
+        // Back to home fragment
+        finish();
+    }
+
+    /**
+     * Validate single user input result.
+     * @param field field result as a String.
+     * @param pattern Regex pattern.
+     * @param errMsg Error message that will be displayed when field validation.
+     *               fails
+     * @return true if valid.
+     */
     private boolean validateField(String field, Pattern pattern, String errMsg) {
         Matcher matcher = pattern.matcher(field);
         if (!matcher.matches()) {
@@ -50,41 +93,18 @@ public class GoalsActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Validates all the user inputs goals view.
+     * @return true if all fields are valid
+     */
     private boolean validate() {
-        Pattern pattern = Pattern.compile("[0-9]");
+        Pattern pattern = Pattern.compile("[0-9]"); // only match numeric values
         String hours = hoursInput.getText().toString();
         String mins = minsInput.getText().toString();
         String cups = caffeineInput.getText().toString();
 
         return validateField(hours, pattern, "Hours value is invalid!") &&
-               validateField(mins,  pattern, "Minutes value is invalid!")  &&
-               validateField(cups,  pattern, "Caffeine value is invalid!");
-    }
-
-
-    private void setSubmitClickHander() {
-        submitBtn.setOnClickListener(v -> {
-            if (!validate()) return;
-
-            int hoursInSeconds = Integer.parseInt(hoursInput.getText().toString()) * 60 * 60;
-            int minsInSeconds = Integer.parseInt(minsInput.getText().toString()) * 60;
-            int totalSeconds = hoursInSeconds + minsInSeconds;
-            int cup = Integer.parseInt(caffeineInput.getText().toString());
-
-            User currentUser = GlobalData.getInstance().getCurrentUser();
-            User newUser = new User(currentUser.getId(), currentUser.getName(), totalSeconds, cup);
-            DbConnection db = new DbConnection(this);
-            String[] params = { Integer.toString(currentUser.getId()) };
-            db.update(DbTables.user.TABLE_NAME, newUser, "id=?", params);
-            db.close();
-
-            // Display success message
-            Toast toast = Toast.makeText(this, "Goals updated", Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL,
-                    0, 190);
-            toast.show();
-
-            finish();
-        });
+                validateField(mins,  pattern, "Minutes value is invalid!")  &&
+                validateField(cups,  pattern, "Caffeine value is invalid!");
     }
 }
